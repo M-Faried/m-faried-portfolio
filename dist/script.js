a5cb0614df29e7189409f7b69c6acdfb298654ea
M Faried <m.a.faried@gmail.com>
2020-04-25 14:38:10 +0200

    The initial draft of my profile

---
+++ dist/script.js
@@ -0,0 +1,12 @@
+// const toggle = document.getElementById('toggle');
+
+// // Toggle nav
+// toggle.addEventListener('click', () => nav.classList.toggle('show-nav'));
+const fieldCards = document.querySelectorAll('.field-card');
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 700) {
+    fieldCards.forEach((card) => card.classList.add('show'));
+  }
+});
