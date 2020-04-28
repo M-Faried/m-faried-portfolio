89e021cded15282361de2b50a096c1a3fea0ca88
M Faried <m.a.faried@gmail.com>
2020-04-28 18:09:04 +0200

    Added the slideup effect

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,12 +1,14 @@
-// const toggle = document.getElementById('toggle');
-
-// // Toggle nav
-// toggle.addEventListener('click', () => nav.classList.toggle('show-nav'));
-const fieldCards = document.querySelectorAll('.field-card');
+const allFieldCards = document.querySelectorAll('.field-card');
 
 window.addEventListener('scroll', () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  if (scrollTop + clientHeight >= scrollHeight - 2000) {
-    fieldCards.forEach((card) => card.classList.add('show'));
-  }
+  const bodyTop = document.body.scrollTop + 5;
+  const documentTop = document.documentElement.scrollTop + 5;
+
+  allFieldCards.forEach((card) => {
+    if (bodyTop > card.offsetTop || documentTop > card.offsetTop) {
+      card.classList.add('slideUp');
+    } else {
+      card.classList.remove('slideUp');
+    }
+  });
 });
