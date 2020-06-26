9633f8cf54fd5f0be8cb770b1369c642318335fd
M Faried <m.a.faried@gmail.com>
2020-06-26 10:59:40 +0200

    Fixing the issue of resources

---
+++ build/precache-manifest.82e53c0511e15e6cf4c161d32e86bda5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab65b4384368f0200833ee96ba88717",
+    "url": "/M-Faried/m-faried-portfolio/index.html"
+  },
+  {
+    "revision": "e1e5a7a2ee485c20aa2a",
+    "url": "/M-Faried/m-faried-portfolio/static/css/main.4f3007f5.chunk.css"
+  },
+  {
+    "revision": "81654151296407d23198",
+    "url": "/M-Faried/m-faried-portfolio/static/js/2.76d076dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/M-Faried/m-faried-portfolio/static/js/2.76d076dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e5a7a2ee485c20aa2a",
+    "url": "/M-Faried/m-faried-portfolio/static/js/main.1c0e0754.chunk.js"
+  },
+  {
+    "revision": "81b04ad168e8984ae71c",
+    "url": "/M-Faried/m-faried-portfolio/static/js/runtime-main.1bca412d.js"
+  }
+]);
