75a5b3882c9b74955d3fdafad51ca37e4e39e65d
M Faried <m.a.faried@gmail.com>
2020-06-27 00:11:21 +0200

    Improved performance and some visual styles'

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/m-faried-portfolio/precache-manifest.395bd988f1c84ca9001ad86e0683d3d6.js"
+  "/m-faried-portfolio/precache-manifest.908ba420bda4e44afd2a96e600b70360.js"
 );
 
 self.addEventListener('message', (event) => {
