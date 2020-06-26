ef120d67e60e88241f5c2a4188c7d78a0df13640
M Faried <m.a.faried@gmail.com>
2020-06-26 09:56:25 +0200

    Second version

---
+++ dist/precache-manifest.5d2b43bc6f5641be5386fe069525afdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f534633e98c275575da0406d13a3f95c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba33575008ae4ff5dd09",
+    "url": "/static/css/main.4f3007f5.chunk.css"
+  },
+  {
+    "revision": "300e216cdb914e714977",
+    "url": "/static/js/2.a5dcc5ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5dcc5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba33575008ae4ff5dd09",
+    "url": "/static/js/main.0b365148.chunk.js"
+  },
+  {
+    "revision": "0ef1522761a03f08cfc9",
+    "url": "/static/js/runtime-main.d1e9b479.js"
+  }
+]);
