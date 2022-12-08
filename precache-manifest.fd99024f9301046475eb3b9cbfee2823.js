00d2b2b99d3ca623598658b5acd177ce54a02fa7
M Faried <m.a.faried@gmail.com>
2022-12-08 18:02:54 +0200

    Updates

---
+++ precache-manifest.fd99024f9301046475eb3b9cbfee2823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9df1fa56407275085730",
+    "url": "/m-faried-portfolio/static/css/main.845fbdca.chunk.css"
+  },
+  {
+    "revision": "9df1fa56407275085730",
+    "url": "/m-faried-portfolio/static/js/main.9df1fa56.chunk.js"
+  },
+  {
+    "revision": "87cf5b82793c522056b3",
+    "url": "/m-faried-portfolio/static/js/1.87cf5b82.chunk.js"
+  },
+  {
+    "revision": "6824927c692908571311",
+    "url": "/m-faried-portfolio/static/js/runtime~main.6824927c.js"
+  },
+  {
+    "revision": "700e04eddd48b1c08b05523abff705be",
+    "url": "/m-faried-portfolio/index.html"
+  }
+];
