e4d1a0ada0fe35091575e5b477790a94ad53c694
M Faried <m.a.faried@gmail.com>
2022-12-09 12:03:51 +0200

    fixed a typo and updated the structure of the project files.

---
+++ build/precache-manifest.083c358eda049c6369f75cf150eded4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25aebe3a975fa920d22e",
+    "url": "/m-faried-portfolio/static/css/main.7a9b8d06.chunk.css"
+  },
+  {
+    "revision": "25aebe3a975fa920d22e",
+    "url": "/m-faried-portfolio/static/js/main.25aebe3a.chunk.js"
+  },
+  {
+    "revision": "402ed4cd91d05be8fd6d",
+    "url": "/m-faried-portfolio/static/js/1.402ed4cd.chunk.js"
+  },
+  {
+    "revision": "6824927c692908571311",
+    "url": "/m-faried-portfolio/static/js/runtime~main.6824927c.js"
+  },
+  {
+    "revision": "8334e0322b5782c2b10a102d8b398f7f",
+    "url": "/m-faried-portfolio/index.html"
+  }
+];
