979656a2efd76cdb44301cf058035d9dcd9dbc59
M Faried <m.a.faried@gmail.com>
2023-01-30 09:40:20 +0200

    Improved the menu for smaller screens.

---
+++ build/precache-manifest.d9bfb02be389c9ec125330fdc208d7d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd14efdc62b8149f13cd",
+    "url": "/m-faried-portfolio/static/css/main.af2df432.chunk.css"
+  },
+  {
+    "revision": "dd14efdc62b8149f13cd",
+    "url": "/m-faried-portfolio/static/js/main.dd14efdc.chunk.js"
+  },
+  {
+    "revision": "d71df150b5ea3bc60fdd",
+    "url": "/m-faried-portfolio/static/js/1.d71df150.chunk.js"
+  },
+  {
+    "revision": "6824927c692908571311",
+    "url": "/m-faried-portfolio/static/js/runtime~main.6824927c.js"
+  },
+  {
+    "revision": "0827c09bbf3d1f252a112babd85a8509",
+    "url": "/m-faried-portfolio/index.html"
+  }
+];
